c2390183c4ee7dc74a2de7398d256c5ef69471ff
한소희 <131546223+HanSo-Hee@users.noreply.github.com>
2023-11-25 18:48:18 +0530

    Update search.py

--- plugins/local_plugins/openai/search.py
+++ plugins/local_plugins/openai/search.py
@@ -3,9 +3,9 @@ from pyrogram import Client, filters
 from plugins.helper.engine import ask_ai
 
 
-@Client.on_message(filters.command('openai'))
+@Client.on_message(filters.command('chatgpt'))
 async def openai_ask(client, message):
     if len(message.command) == 1:
-       return await message.reply_text("Give an input!")
+       return await message.reply_text("Give me a prompt to Search ✨")
     m = await message.reply_text("👀")
     await ask_ai(client, m, message)
