ab0c8c3714050ccf385f83f3e29a2eaec3b15844
한소희 <131546223+HanSo-Hee@users.noreply.github.com>
2023-11-25 18:38:11 +0530

    Update password.py

--- plugins/Extra/password.py
+++ plugins/Extra/password.py
@@ -3,9 +3,9 @@ from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
-@Client.on_message(filters.command(["genpassword", 'genpw']))
+@Client.on_message(filters.command(["genpassword", 'genpw', 'genpass']))
 async def password(bot, update):
-    message = await update.reply_text(text="`Processing...`")
+    message = await update.reply_text(text="`Generating the Password...`")
     password = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+".lower()
     if len(update.command) > 1:
         qw = update.text.split(" ", 1)[1]
@@ -15,5 +15,5 @@ async def password(bot, update):
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
     txt = f"<b>Limit:</b> {str(limit)} \n<b>Password: <code>{random_value}</code>"
-    btn = InlineKeyboardMarkup([[InlineKeyboardButton('Team Rio™️', url='https://t.me/Teamrioupdates')]])
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton('✨ Updates', url='https://t.me/SingleMachiNetworks')]])
     await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
