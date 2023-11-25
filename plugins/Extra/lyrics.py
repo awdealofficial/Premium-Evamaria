45fbc85c83903e3998ced96365a3b6abdc3ca6e6
한소희 <131546223+HanSo-Hee@users.noreply.github.com>
2023-11-25 18:35:54 +0530

    Update lyrics.py

--- plugins/Extra/lyrics.py
+++ plugins/Extra/lyrics.py
@@ -13,16 +13,16 @@ async def sng(bot, message):
         if not message.reply_to_message:
           await message.reply_text("Please reply to a message")
         else:          
-          mee = await message.reply_text("`Searching 🔎`")
+          mee = await message.reply_text("`Searching the lyrics 🔎`")
           song = message.reply_to_message.text
           chat_id = message.from_user.id
           rpl = lyrics(song)
           await mee.delete()
           try:
             await mee.delete()
-            await bot.send_message(chat_id, text = rpl, reply_to_message_id = message.id, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs ", url = f"t.me/mkn_bots_updates")]]))
+            await bot.send_message(chat_id, text = rpl, reply_to_message_id = message.id, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Uᴘᴅᴀᴛᴇs ", url = f"t.me/TeleRoidGroup")]]))
           except Exception as e:                            
-             await message.reply_text(f"I Can't Find A Song With `{song}`", quote = True, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url = f"t.me/mkn_bots_updates")]]))
+             await message.reply_text(f"I Can't Find A Song With `{song}`", quote = True, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Uᴘᴅᴀᴛᴇs", url = f"t.me/TeleRoidGroup")]]))
 
 
 def search(song):
@@ -34,7 +34,7 @@ def lyrics(song):
         fin = search(song)
         text = f'**🎶 Sᴜᴄᴄᴇꜱꜰᴜʟʟy Exᴛʀᴀᴄᴛᴇᴅ Lyɪʀɪᴄꜱ Oꜰ {song}**\n\n'
         text += f'`{fin["lyrics"]}`'
-        text += '\n\n\n**Made By Artificial Intelligence**'
+        text += '\n\n\n**Thanks to @Xditya © @KDramasFlix**'
         return text
 
 
