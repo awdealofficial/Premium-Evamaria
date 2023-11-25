9ff878d1acd23d85b9052b025382491a9a16bfc4
한소희 <131546223+HanSo-Hee@users.noreply.github.com>
2023-11-25 18:30:32 +0530

    Update check_alive.py

--- plugins/Extra/check_alive.py
+++ plugins/Extra/check_alive.py
@@ -6,7 +6,7 @@ CMD = ["/", "."]
 
 @Client.on_message(filters.command("alive", CMD))
 async def check_alive(_, message):
-    await message.reply_text("you very luck 🤞 iam alive ❤️ press /start use me")
+    await message.reply_text("You want me to Die, But Your BadLuck I'm alive ❤️‍🔥 Check My Ping /ping .")
 
 
 @Client.on_message(filters.command("ping", CMD))
