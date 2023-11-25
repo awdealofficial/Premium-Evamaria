75b214b45a8a5a47f113d2e41b9f46fa4aa48104
한소희 <131546223+HanSo-Hee@users.noreply.github.com>
2023-11-25 19:03:48 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -35,7 +35,7 @@ async def answer(bot, query):
     if AUTH_CHANNEL and not await is_subscribed(bot, query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='You have to subscribe my channel to use the bot',
+                           switch_pm_text='Join My Updates Channel to Use Me',
                            switch_pm_parameter="subscribe")
         return
 
@@ -107,11 +107,8 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query)
+            InlineKeyboardButton('🔎 𝖲𝖾𝖺𝗋𝖼𝗁 𝖠𝗀𝖺𝗂𝗇', switch_inline_query_current_chat=query),
+            InlineKeyboardButton('⚡𝖴𝗉𝖽𝖺𝗍𝖾𝗌 ⚡', url="https://t.me/MoviesFlixers_DL")
         ]
         ]
     return InlineKeyboardMarkup(buttons)
-
-
-
-
